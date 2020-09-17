a91f6909ec5dd58874979ad05486f922b94e888b
diego-kurman <diegoandradekurman@gmail.com>
2020-09-17 19:44:40 -0300

    atualizando o banco para a lingua inglesa

--- documentação/Banco de dados/Script do Banco/Script - V1.sql
+++ documentação/Banco de dados/Script do Banco/Script - V1.sql
@@ -1,43 +1,48 @@
-CREATE TABLE Estabelecimento(
-	idEstabelecimento INT PRIMARY KEY IDENTITY(1,1),
-	cidadeEstabelecimento VARCHAR(45)
+CREATE TABLE Establishment(
+	idEstablishment INT PRIMARY KEY IDENTITY(1,1),
+	city VARCHAR(45),
+    zipCode CHAR (9),
+    street VARCHAR (45),
+    EstablishmentNumber varchar (5)
+    
+    
 );
 
 CREATE TABLE Usuario(
-	idUsuario INT PRIMARY KEY IDENTITY(1,1),
-	nomeUsuario VARCHAR(45),
-	loginUsuario VARCHAR(45),
-	senhaUsuario VARCHAR(45),
-	fkestabelecimentoUsuario INT,
-	fOREIGN KEY(fkestabelecimentoUsuario) REFERENCES Estabelecimento(idEstabelecimento)
+	idUser INT PRIMARY KEY IDENTITY(1,1),
+	nameUser VARCHAR(45),
+	loginUser VARCHAR(45),
+	passwordUser VARCHAR(45),
+	fkestablishmentUser INT,
+	fOREIGN KEY(fkEstablishmentUser) REFERENCES Establishment(idEstablishment)
 );
 
-CREATE TABLE Maquina (
-	idMaquina INT PRIMARY KEY IDENTITY(1,1),
-	cpuMaquina TINYINT,
-	memoriaMaquina TINYINT,
-	discoMaquina TINYINT,
-	fkestabelecimentoMaquina INT,
-	fOREIGN KEY(fkestabelecimentoMaquina) REFERENCES Estabelecimento(idEstabelecimento)
+CREATE TABLE Machine (
+	idMachine INT PRIMARY KEY IDENTITY(1,1),
+	processadorMachine TINYINT,
+	memoryMachine TINYINT,
+	discoMachine TINYINT,
+	fkEstablishmentMachine INT,
+	fOREIGN KEY(fkEstablishmentMachine) REFERENCES Establishment(idEstablishment)
 
 );
 
-CREATE TABLE Teste(
-	idTeste INT PRIMARY KEY IDENTITY(1,1),
-	dataHora DATETIME,
-	cpuTeste TINYINT,
-	memoriaTeste TINYINT,
-	discoTeste TINYINT,
-	pintgTeste TINYINT,
-	fkestabelecimentoTeste INT,
-	fOREIGN KEY(fkestabelecimentoTeste) REFERENCES Estabelecimento(idEstabelecimento)
+CREATE TABLE Test(
+	idTest INT PRIMARY KEY IDENTITY(1,1),
+	dtMoment DATETIME,
+	cpuTest TINYINT,
+	memoryTest TINYINT,
+	discTest TINYINT,
+	pingTest TINYINT,
+	fkEstablishmentTest INT,
+	fOREIGN KEY(fkTestMachine) REFERENCES Machine(idMachine)
 );
-CREATE TABLE Dados (
-	idDados INT PRIMARY KEY IDENTITY(1,1),
-	dataHora DATETIME,
-	cpuDados TINYINT,
-	memoriaDados TINYINT,
-	discoDados TINYINT,
-	fkestabelecimentoDados INT,
-	fOREIGN KEY(fkestabelecimentoDados) REFERENCES Estabelecimento(idEstabelecimento)
+CREATE TABLE Datas (
+	idDatas INT PRIMARY KEY IDENTITY(1,1),
+	dtMoment DATETIME,
+	cpuData TINYINT,
+	menoryData TINYINT,
+	discData TINYINT,
+	fkEstablishmentData INT,
+	fOREIGN KEY(fkDatasMachine) REFERENCES Machine(idMachine)
 );
