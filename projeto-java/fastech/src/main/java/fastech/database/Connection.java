d017fa0e8180ab8a6dd186584c5cad3c790496af
jorge-lsb <jorge.barbosa@bandtec.com.br>
2020-11-19 18:04:25 -0300

    Ajustes na classe aluno

--- projeto-java/fastech/src/main/java/fastech/database/Connection.java
+++ projeto-java/fastech/src/main/java/fastech/database/Connection.java
@@ -1,15 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package fastech.database;
 
 import org.apache.commons.dbcp2.BasicDataSource;
 
 /**
  *
- * @author Cliente
+ * @author Jorge
  */
 public class Connection {
     
