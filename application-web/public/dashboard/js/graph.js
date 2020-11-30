dbbbbe275be1ce0f6e42f873a9c99f1b67d5e9a9
jose-lucas-mq <jose.queiroz@bandtec.com.br>
2020-11-29 23:46:38 -0300

    new dash with warning

--- application-web/public/dashboard/js/graph.js
+++
@@ -1,8 +0,0 @@
-function circle(el){
-    $(el).circleProgress({fill: {color: '#3B5998'}})
-    .on('circle-animation-progress', function(event, progress, stepValue){
-        $(this).find('strong').text(String(stepValue.toFixed(2) * 100) + '%');
-    });
-}
-
-circle('.round');
