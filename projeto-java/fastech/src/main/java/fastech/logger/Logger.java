c24b3397509d8f68786445bd1cb91d940256d937
jorge-lsb <jorge.barbosa@bandtec.com.br>
2020-12-02 21:36:29 -0300

    Adicionando novas funcionalidades no controller

--- projeto-java/fastech/src/main/java/fastech/logger/Logger.java
+++ projeto-java/fastech/src/main/java/fastech/logger/Logger.java
@@ -1,16 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package fastech.logger;
 
 import fastech.services.TakingDataServices;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Date;
-
 
 /**
  *
@@ -19,21 +12,29 @@ import java.util.Date;
 public class Logger {
 
     TakingDataServices dataServices = new TakingDataServices();
-    private File arquivo;
+    private File file;
 
     public Logger() {
-        
-        
-        arquivo = new File("log.txt");
+        file = new File("log.log");
     }
-    
-    
-    public void escreveTexto(String tipo, String recebe) throws IOException{
-        FileWriter inserindo = new FileWriter(arquivo, true);
-        inserindo.write(String.format("\n %s - %s - %s", tipo, dataServices.dateNow(), recebe));
-        inserindo.close();
+
+    public void loggingIn(String user) throws IOException {
+        FileWriter insert = new FileWriter(file, true);
+        insert.write("          STARTING REGISTRATION SESSION");
+        insert.write("\n-----------------------------------------------");
+        insert.write("\n-----------------------------------------------");
+        insert.write(String.format("\nMachine processor: %s", dataServices.getNameProcessor()));
+        insert.write(String.format("\nActive user: %s", user));
+        insert.write(String.format("\nSession date: %s", dataServices.dateNow()));
+        insert.write("\n-----------------------------------------------");
+
+        insert.close();
+    }
+
+    public void registerLog(String type, String error) throws IOException {
+        FileWriter insert = new FileWriter(file, true);
+        insert.write(String.format("\n %s - %s - %s", dataServices.dateNow(), type, error));
+        insert.close();
     }
 
 }
-    
-
