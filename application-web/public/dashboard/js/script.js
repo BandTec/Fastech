63ce8dee1802d96783b245b67db6eebbdf1de261
jose-lucas-mq <jose.queiroz@bandtec.com.br>
2020-12-05 20:01:04 -0300

    Recuperando maquinas do usuario

--- application-web/public/dashboard/js/script.js
+++ application-web/public/dashboard/js/script.js
@@ -4,29 +4,60 @@ let var_cpf;
 let var_login = sessionStorage.user_login;
 let var_office;
 let var_company;
+let var_id_company;
 
 
 function setVars() {
     fetch(`/usuarios/user/${var_login}`, { cache: 'no-store' })
-    .then(res => {
-        if(res.ok) {
-            res.json().then((json) => {
-                var_company = json.NameCompany;
-                var_cpf = json.cpf;
-                var_name = json.name;
-                var_office = json.office;
-                loginUser();
-            })
-        } else {
-            logoff();
-        }
-    });
+        .then(res => {
+            if (res.ok) {
+                res.json().then((json) => {
+                    var_company = json.NameCompany;
+                    var_cpf = json.cpf;
+                    var_name = json.name;
+                    var_office = json.office;
+                    var_id_company = json.idCompany;
+                    loginUser();
+                })
+            } else {
+                logoff();
+            }
+        });
+}
+
+function showMachine() {
+    fetch(`/data/machine/${var_id_company}`, { cache: 'no-store' })
+        .then(res => {
+            if (res.ok) {
+                res.json().then((json) => {
+                    for(let i = 0; i < json.length; i++){
+
+                        machines.innerHTML += `
+                        <div class="col-md-4 col-sm-12">
+                            <div class="card" id="status_good" data-toggle="modal" data-target="#machineModal">
+                                <div class="card-body">
+                
+                                    <div class="card-title">
+                                        ${json[i].nameMachine}
+                                    </div>
+                
+                                    <i class="fas fa-plus"></i><span
+                                        class="sr-only">Redirecionar</span>
+                                </div>
+                            </div>
+                        </div>
+                    `;
+                    }
+                })
+            }
+        });
 }
 
 function loginUser() {
     validateUser();
     validar_sessao();
     userPrint();
+    showMachine();
 }
 
 function validateUser() {
@@ -36,7 +67,7 @@ function validateUser() {
 }
 
 function userPrint() {
-   
+
     user_name.innerHTML = var_name;
     user.innerHTML = var_name;
     email.innerHTML = var_login;
@@ -70,5 +101,5 @@ function validar_sessao() {
 }
 
 function finalizar_sessao() {
-    fetch(`/usuarios/sair/${user_login}`, { cache: 'no-store' });
+    fetch(`/usuarios/sair/${var_login}`, { cache: 'no-store' });
 }
---
+++ application-web/routes/data.js
@@ -0,0 +1,30 @@
+const { json } = require('express');
+var express = require('express');
+var router = express.Router();
+var sequelize = require('../models').sequelize;
+var Usuario = require('../models').Usuario;
+
+
+/* Mostrando as maquinas de acordo com a empresa */
+router.get('/machine/:company', (req,res,next) => {
+	let companyId = req.params.company;
+	
+    let machineSelect = `SELECT m.Name as nameMachine, m.idMachine AS 'IdMachine' FROM Machine m, CompanyBranch cb 
+    WHERE m.fkCompanyBranch = cb.idCompanyBranch AND cb.idCompanyBranch = ${companyId}`;
+
+	sequelize.query(machineSelect, {
+		model: Usuario
+	}).then(resultado => {
+		console.log(`Encontrados: ${resultado.length}`);
+		if (resultado.length > 0) {
+           
+            res.json(resultado);   		
+		} 
+	}).catch(erro => {
+		console.error(erro);
+		res.status(500).send(erro.message);
+	});
+});
+
+
+module.exports = router;
